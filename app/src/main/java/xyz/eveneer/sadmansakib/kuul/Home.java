531f9143e4df4d98d1f337c879c82d4ce20a0af5
sadmansakib <ssadman8@gmail.com>
2018-12-20 01:50:08 +0600

    initial repo

---
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/Home.java
@@ -0,0 +1,13 @@
+package xyz.eveneer.sadmansakib.kuul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
