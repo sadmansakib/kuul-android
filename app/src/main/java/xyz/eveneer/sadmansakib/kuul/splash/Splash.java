536a6f5825091191ffd43d07072f63a05a5195e9
sadmansakib <ssadman8@gmail.com>
2018-12-20 17:54:58 +0600

    add splash Activity as launcher activity

---
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/splash/Splash.java
@@ -0,0 +1,15 @@
+package xyz.eveneer.sadmansakib.kuul.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.eveneer.sadmansakib.kuul.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
