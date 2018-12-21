f559b98bd4d9c059f587d56dfb7a7c627cd64736
sadmansakib <ssadman8@gmail.com>
2018-12-21 15:39:01 +0600

    splash transparent bar

--- app/src/main/java/xyz/eveneer/sadmansakib/kuul/splash/Splash.java
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/splash/Splash.java
@@ -2,6 +2,8 @@ package xyz.eveneer.sadmansakib.kuul.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import xyz.eveneer.sadmansakib.kuul.R;
 
@@ -11,5 +13,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        /*
+          Transparent status bar for splash screen
+          **/
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
