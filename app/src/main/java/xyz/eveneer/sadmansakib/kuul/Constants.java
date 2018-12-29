ab4a26d613bac703d51115fa6a94dacb0c37d9b7
sadmansakib <ssadman8@gmail.com>
2018-12-29 15:58:52 +0600

    initiate: add splash screen

---
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/Constants.java
@@ -0,0 +1,7 @@
+package xyz.eveneer.sadmansakib.kuul;
+
+public class Constants{
+    public static class splash{
+        public static final int SPLASH_DELAY = 1500;
+    }
+}
---
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/Splash/Splash.java
@@ -0,0 +1,42 @@
+package xyz.eveneer.sadmansakib.kuul.Splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.eveneer.sadmansakib.kuul.Home.Home;
+import xyz.eveneer.sadmansakib.kuul.R;
+
+import static xyz.eveneer.sadmansakib.kuul.Constants.splash.SPLASH_DELAY;
+
+public class Splash extends AppCompatActivity {
+
+    private final Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(this::launch,SPLASH_DELAY);
+    }
+
+    @Override
+    protected void onStop() {
+        mHandler.removeCallbacks(this:: launch);
+        super.onStop();
+    }
+
+    void launch() {
+        if (!isFinishing()) {
+            startActivity(new Intent(getApplicationContext(), Home.class));
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+            finish();
+        }
+    }
+}
