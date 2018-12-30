4db778691b7c8256dcf37c5e40b54e2557b9dbf6
sadmansakib <ssadman8@gmail.com>
2018-12-30 20:55:27 +0600

    initiate licensing for Kuul
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/xyz/eveneer/sadmansakib/kuul/Tutorial/TutorialViewModel.java
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/Tutorial/TutorialViewModel.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *    Copyright 2018  Sadman Sakib
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package xyz.eveneer.sadmansakib.kuul.Tutorial;
 
 import android.arch.lifecycle.ViewModel;
@@ -10,9 +28,9 @@ import xyz.eveneer.sadmansakib.kuul.R;
 
 import static xyz.eveneer.sadmansakib.kuul.Constants.tutorial.PERMISSIONS;
 
-public class TutorialViewModel extends ViewModel {
+class TutorialViewModel extends ViewModel {
 
-    public void setupview(TutorialActivity activity, Context context){
+    void setupview(TutorialActivity activity, Context context){
         activity.addFragment(new PermissionStep.Builder().setTitle(context.getString(R.string.app_name))
                 .setContent(context.getString(R.string.tutorial_p1_content))
                 .setBackgroundColor(context.getResources().getColor(R.color.bg_color)) // int background color
