5a9768635cbec442451c6983d4dc575515ee0316
sadmansakib <ssadman8@gmail.com>
2019-01-01 01:16:31 +0600

    Integrated SignUp request system with phone number from sqlite.
    we no longer have to fetch phone number from accountkit for registering
    an user
    
    Signed-off-by: sadmansakib <ssadman8@gmail.com>

--- app/src/main/java/xyz/eveneer/sadmansakib/kuul/SignUp/SignUp.java
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/SignUp/SignUp.java
@@ -1,15 +1,52 @@
 package xyz.eveneer.sadmansakib.kuul.SignUp;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 
 import xyz.eveneer.sadmansakib.kuul.R;
 
 public class SignUp extends AppCompatActivity {
 
+    private  SignUpViewModel signUpViewModel;
+
+    EditText gender,name,address;
+
+    AppCompatButton loginbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signUpViewModel=ViewModelProviders.of(this).get(SignUpViewModel.class);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        loginbtn = findViewById(R.id.login_button);
+        gender=findViewById(R.id.gender);
+        name= findViewById(R.id.name);
+        address=findViewById(R.id.address);
+
+        loginbtn.setOnClickListener(v ->
+                signUpViewModel.getUserNumber().observe(this, number -> signUpViewModel.login(
+                        this,
+                name.getText().toString().trim(),
+                number,
+                gender.getText().toString().trim(),
+                address.getText().toString().trim())
+        ));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
---
+++ app/src/main/java/xyz/eveneer/sadmansakib/kuul/SignUp/SignUpViewModel.java
@@ -0,0 +1,85 @@
+/*
+ *
+ *    Copyright 2018  Sadman Sakib
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package xyz.eveneer.sadmansakib.kuul.SignUp;
+
+import android.app.Activity;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import xyz.eveneer.sadmansakib.kuul.Data.Dao.PhoneNumberDao;
+import xyz.eveneer.sadmansakib.kuul.Data.DataBase.PhoneNumberRoomDatabase;
+import xyz.eveneer.sadmansakib.kuul.Home.Home;
+import xyz.eveneer.sadmansakib.kuul.Kuul;
+import xyz.eveneer.sadmansakib.kuul.Models.Register;
+import xyz.eveneer.sadmansakib.kuul.R;
+
+
+public class SignUpViewModel extends AndroidViewModel {
+
+    private String TAG = getClass().getSimpleName();
+
+    private LiveData<String> number;
+
+    public SignUpViewModel(@NonNull Application application) {
+        super(application);
+        PhoneNumberRoomDatabase phoneDB = PhoneNumberRoomDatabase.getDatabase(application);
+        PhoneNumberDao phoneDao = phoneDB.phoneNumberDao();
+        number= phoneDao.getUserNumber();
+    }
+
+    LiveData<String> getUserNumber() {
+        return number;
+    }
+
+    void login(Activity activity, String name, String number, String gender, String address) {
+        Call<Register> call= Kuul.getClient().registerUser(name,number,gender,address);
+        call.enqueue(new Callback<Register>() {
+            @Override
+            public void onResponse(@NonNull Call<Register> call, @NonNull Response<Register> response) {
+                if(Objects.requireNonNull(response.body()).getStatus().contains("success")){
+                    reDirectToHome();
+                    activity.overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+                } else{
+                    Log.e(TAG, "onResponse: "+response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Register> call, @NonNull Throwable t) {
+                Log.e(TAG, "onFailure: ", t.getCause());
+            }
+        });
+    }
+
+    private void reDirectToHome() {
+        getApplication().
+                getApplicationContext().
+                startActivity(new Intent(getApplication().
+                        getApplicationContext(),Home.class));
+    }
+}
